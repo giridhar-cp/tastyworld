7699bb3e77dd92daf276483109fdf86c12c9d500
Giridhar C P <giridhar@cse.carmelcet.in>
2025-01-12 18:59:23 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //config variables
 const currency = "eur";
 const deliveryCharge = 1.5;
-const frontend_URL = 'http://localhost:5173';
+const frontend_URL = 'https://tastyworld-frontend.onrender.com/';
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
