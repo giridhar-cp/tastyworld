09d4e21c1b8fede47230a05ce57df3446c5b325c
Giridhar C P <giridhar@cse.carmelcet.in>
2025-01-13 15:21:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,13 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
 app.use(cors())
 
 // db connection
