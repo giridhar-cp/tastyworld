2896b032bdf2938696306489c039aab33d50e8f7
Giridhar C P <giridhar@cse.carmelcet.in>
2025-02-03 23:19:45 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+/*import React, { useEffect, useState } from 'react'
 import './List.css'
 import { url, currency } from '../../assets/assets'
 import axios from 'axios';
@@ -63,3 +63,78 @@ const List = () => {
 }
 
 export default List
+*/
+
+import React, { useEffect, useState } from 'react';
+import './List.css';
+import { url, currency } from '../../assets/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${url}/api/food/list`);
+      if (response.data.success) {
+        setList(response.data.data);
+      } else {
+        toast.error('Error fetching food list');
+      }
+    } catch (error) {
+      toast.error('An error occurred while fetching food list');
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, {
+        id: foodId,
+      });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        fetchList();  // Re-fetch the food list after removing an item
+      } else {
+        toast.error('Error removing food');
+      }
+    } catch (error) {
+      toast.error('An error occurred while removing food');
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => (
+          <div key={index} className="list-table-format">
+            <img src={`${url}${item.image}`} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p className="cursor" onClick={() => removeFood(item._id)}>
+              x
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
