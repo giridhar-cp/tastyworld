4e3c67c724b9152dfb5cc9eb01564dcfe13e0c4a
Giridhar C P <giridhar@cse.carmelcet.in>
2025-02-08 09:28:07 +0530

    Update foodRoute.js

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
 const foodRouter = express.Router();
@@ -19,3 +19,17 @@ foodRouter.post("/add",upload.single('image'),addFood);
 foodRouter.post("/remove",removeFood);
 
 export default foodRouter;
+*/
+import express from 'express';
+import { addFood, listFood, removeFood } from '../controllers/foodController.js';
+import multer from 'multer';
+import { storage } from '../config/cloudinary.js';
+
+const upload = multer({ storage });
+const router = express.Router();
+
+router.get('/list', listFood);
+router.post('/add', upload.single('image'), addFood);
+router.post('/remove', removeFood);
+
+export default router;
